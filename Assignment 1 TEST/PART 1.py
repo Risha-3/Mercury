7137b979ba33e643db5b143cdf0bddbf7c1e396d
Risha-3 <rs989@kent.ac.uk>
2024-10-14 18:52:44 +0000

    Completed PART 1

--- Assignment 1 TEST/PART 1.py
+++ Assignment 1 TEST/PART 1.py
@@ -1,1 +1,11 @@
-
+weather = input("What is the weather like right now? ").lower()
+if weather == "sunny":
+    print("It's sunny. Enjoy the sunshine!")
+elif weather == "cloudy":
+    print("It's cloudy. It might stay dry, but be prepared for changes.")
+elif weather == "rainy":
+    print("It's rainy. You might want to carry an umbrella with you.")
+elif weather == "snowy":
+    print("It's snowy. Be cautious while driving and dress warmly.")
+else:
+    print("I'm sorry, I don't have a specific recommendation")
