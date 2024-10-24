1614141368023d410e47c8cd4953513bb7f9e464
Risha-3 <rs989@kent.ac.uk>
2024-10-24 10:43:36 +0000

    Completed WHILE LOOP task

---
+++ Assignment 2 TEST/While Loops.py
@@ -0,0 +1,13 @@
+import random
+number = random.randint(1,5)
+check = 0
+while check == 0:
+    guessed_number = int(input("Enter a number between 1 and 100: "))
+    if 1 <= guessed_number <= 100:
+        if guessed_number == number:
+            print("You have guessed correctly.")
+            check += 1
+        else:
+            print("Please try again.")
+    else:
+        print("You must enter an integer between 1 an 100.")
