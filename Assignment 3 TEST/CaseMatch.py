a340178de052594530d914e600aa267bdbfa2a1c
Risha-3 <rs989@kent.ac.uk>
2024-11-05 17:25:20 +0000

    progress 3

--- Assignment 3 TEST/CaseMatch.py
+++ Assignment 3 TEST/CaseMatch.py
@@ -1,4 +1,8 @@
 def fix_robot():
+    """
+    This function provides a brief guide on basic maintenance tasks depending on what type of robot you own.
+    It prompts the user to select the type of robot that requires maintenance and then outputs the corresponding instructions.
+    """
     print("***A very short guide on how to fix a robot***")
     print("""
           What type of robot do you have:
@@ -15,10 +19,19 @@ def fix_robot():
                 print("Empty rubbish, Hard reset, check for any updates.")
             case 3:
                 print("Clean sensors, Charge batteries, Check for firmare updates, Perform software updates.")
-            case _:
+            case _:  # Default case for invalid input (if the user enters an integer other than 1, 2 or 3).
                 print("Invalid selection. Please try again.")
+    
     except ValueError:
+    # If the user inputs anything other than a valid integer, rather than the program displaying red text, it will raise a ValueError.
+    # By doing this, I will be able to inform the user that the input was invalid without crashing the program.
         print("Invalid input! Please enter a number (1, 2, or 3).")
 
 
 fix_robot()
+
+# Strings and Intgers
+# Inputs and Outputs
+# Definitions
+# Match-Case Statements
+# Error Handling
--- Assignment 3 TEST/MintoHrs.py
+++ Assignment 3 TEST/MintoHrs.py
@@ -1,4 +1,8 @@
 def min_to_hrs(user_minutes):
+    """
+    This function converts a given number of minutes into hours and minutes.
+    It then returns a tuple containing two elements which is the number of hours and the remaining minutes. 
+    """
     hours = int(user_minutes // 60)
     minutes = user_minutes % 60
     return hours, minutes
@@ -17,3 +21,11 @@ while True:
         print(f"{user_minutes} minutes is equivalent to {hours} hours and {minutes:.2f} minutes.")
     except ValueError:
         print("Invalid input. Please try again.")
+
+
+#Strings,Integers and Floats
+#Inputs and outputs
+#Definitions
+#Conditionals
+#While loop
+#Error handling
