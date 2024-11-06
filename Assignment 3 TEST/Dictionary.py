451b17938a931ab0860486b6b3aa9049aafe78ea
Risha-3 <rs989@kent.ac.uk>
2024-11-06 17:54:13 +0000

    Complete

--- Assignment 3 TEST/Dictionary.py
+++ Assignment 3 TEST/Dictionary.py
@@ -12,7 +12,7 @@ user_watchlist = {
         "Platform": "Disney+",
         "Status": "Not Watched",
     },
-    "Tulsa King": {
+    "TULSA KING": {
         "Platform": "Amazon Prime",
         "Status": "Watching",
     }
@@ -23,9 +23,17 @@ def add_entry():
     This function asks the user for the title, platform and status of the tv show/movie.
     These details are then added to the user_watchlist dictionary.
     """
-    title = input("What is the title of the TV show / Movie: ").upper()
-    platform = input("What is the streaming platform: ")
-    status = input("Status (Not Watched, Watching, Completed): ")
+    title = input("What is the title of the TV show / Movie: ").strip().upper()  # The title is converted to upper case, to keep it consistent with the entries that are already in the dictionary.
+    platform = input("What is the streaming platform: ").strip()
+
+    while True:
+        status = input("Status (Not Watched, Watching, Completed): ").strip().lower() # Getting rid of white spaces before and after entry as well as making all the letters lower case.
+        if status in ["not watched", "watching", "completed"]:  # Making sure that one of the three acceptable solutions have been entered.
+            status = status.title()  # Making the start of each word upper case.
+            break  # If the check is successfully complete, break loop so that entry can be added to dictionary.
+        else:
+            print("Only 'Not watched', 'Watching' and 'Completed' is accepted. Please try again.")
+
     user_watchlist[title] = {
         "Platform": platform,
         "Status": status,
@@ -38,22 +46,32 @@ def update_status():
     If the title is in the watchlist the program will ask for the new status.
     If the title is not in the watchlist, an appropriate message is output.
     """
-    title = input("What is the title of the TV show / Movie that needs to be updated: ").upper()
+    title = input("What is the title of the TV show / Movie that needs to be updated: ").strip().upper()
     if title in user_watchlist:
-        new_status = input("New status (Not Watched, Watching, Completed): ")
-        user_watchlist[title]["Status"] = new_status
-        print(f"Updated status of {title} to {new_status}.")
+        while True:
+            new_status = input("New status (Not Watched, Watching, Completed): ").strip().lower()
+            if new_status in ["not watched", "watching", "completed"]:  # Making sure that one of the three acceptable solutions have been entered.
+                user_watchlist[title]["Status"] = new_status.title()  # Replacing current status with new status.
+                print(f"Updated status of {title} to {new_status.title()}.")
+                break
+            else:
+                print("Only 'Not watched', 'Watching' and 'Completed' is accepted. Please try again.")
     else:
+        # If the user selects the update action and the user's entry is not in the watchlist, this message will be output.
         print("The title of the TV show / Movie is not in the watchlist.")
+
 
 def display_watchlist():
     """
     This function iterates through the user_watchlist dictionary and prints each title along with its details.
     """
-    for title, details in user_watchlist.items():
-        print(f"\nTitle: {title}")
-        for key, value in details.items():
-            print(f"  {key}: {value}")
+    if user_watchlist:
+        for title, info in user_watchlist.items():
+            print(f"\nTitle: {title}")
+            for key, value in info.items():
+                print(f"  {key}: {value}")
+    else:
+        print("Nothing to display. Your watchlist is empty.")
 
 
 # This is the main menu screen that will continuously ask the user to select an option until they type 'quit'.
