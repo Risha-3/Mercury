43e121beaa50a14073093b92c921bed0381352ed
Risha-3 <rs989@kent.ac.uk>
2024-11-14 20:57:06 +0000

    Complete

--- Assignment 3 TEST/dinosaur.py
+++ Assignment 3 TEST/dinosaur.py
@@ -5,13 +5,14 @@ from weatherQuery import get_weather
 api_key = 'f3d6d1f6c418203d3d2c727199918c83'
 
 def trex_weather(city):
+    """
+
+    """
     weather_data = get_weather(city, api_key)
-    
     if weather_data:
         temperature = weather_data['main']['temp']
         humidity = weather_data['main']['humidity']
         description = weather_data['weather'][0]['description']
-            
         message = (
             f"Weather in {city}:\n"
             f"Temperature: {temperature}°C\n"
@@ -29,4 +30,4 @@ if len(sys.argv) != 2:
 city = sys.argv[1]
 trex_weather(city)
 
-# /home/codespace/.python/current/bin/python3 "/workspaces/Mercury/Assignment 3 TEST/dinosaur.py" "Kent"
+# /home/codespace/.python/current/bin/python3 "/workspaces/Mercury/Assignment 3 TEST/dinosaur.py" "Canterbury"
