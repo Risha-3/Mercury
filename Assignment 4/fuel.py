7b1f55a2e490b59d2052a6fc08166d616064ef33
Risha-3 <rs989@kent.ac.uk>
2024-11-29 15:49:05 +0000

    saving corrections

--- Assignment 4/fuel.py
+++ Assignment 4/fuel.py
@@ -1,24 +1,23 @@
 def main():
   fraction = input("Enter a fraction as X/Y: ")
   percentage = convert(fraction)
-  result = gauge(percentage)
-  print(f"percentage: {percentage}")
-  print(f"Gauge: {result}")
+  gauge_reading = gauge(percentage)
+  print(f"Gauge: {gauge_reading}")
   
 def convert(fraction):
     numerator, denominator = fraction.split("/")
     numerator = int(numerator)
     denominator = int(denominator)
-    percentage = round((numerator/denominator)*100)
+    percentage = round((numerator / denominator) * 100)
     return percentage
 
 def gauge(percentage):
     if percentage <= 1:
-        return "E"
+        return "E" 
     elif percentage >= 99:
        return "F"
     else:
-       return "Z%"
+       return f"{percentage}%"
 
 if __name__ == "__main__":
- main()
+    main()
