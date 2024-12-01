8aaa1f64c91497b170fd9850c9ec4f6c7b3ee4c3
Risha-3 <rs989@kent.ac.uk>
2024-12-01 21:49:58 +0000

    Update fuel.py

--- Assignment 4 TEST/fuel.py
+++ Assignment 4 TEST/fuel.py
@@ -1,7 +1,7 @@
 def main():
   """
   This function asks the user to enter a fraction (X/Y).
-  Converts fraction to a percentage using the convert function
+  Converts fraction to a percentage using the convert function.
   Using the gauge function, an appropriate gauge reading is displayed based on the percentage. 
   """
   fraction = input("Enter a fraction as X/Y: ")
@@ -19,7 +19,7 @@ def main():
   
 def convert(fraction):
   """
-  Convert the user input (X/Y) into a percentage
+  Convert the user input (X/Y) into a percentage.
   """
   # The input X/Y is split into numerator and denominator.
   numerator, denominator = fraction.split("/")
