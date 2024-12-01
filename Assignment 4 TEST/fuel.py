c694ccd68458eb7601c0d48c98420df73898b2e2
Risha-3 <rs989@kent.ac.uk>
2024-12-01 13:59:15 +0000

    changes

--- Assignment 4 TEST/fuel.py
+++ Assignment 4 TEST/fuel.py
@@ -9,21 +9,21 @@ def main():
 def convert(fraction):
   """
   """
-    numerator, denominator = fraction.split("/")
-    numerator = int(numerator)
-    denominator = int(denominator)
-    percentage = round((numerator / denominator) * 100)
-    return percentage
+  numerator, denominator = fraction.split("/")
+  numerator = int(numerator)
+  denominator = int(denominator)
+  percentage = round((numerator / denominator) * 100)
+  return percentage
 
 def gauge(percentage):
   """
   """
-    if percentage <= 1:
-        return "E" 
-    elif percentage >= 99:
-       return "F"
-    else:
-       return f"{percentage}%"
+  if percentage <= 1:
+    return "E" 
+  elif percentage >= 99:
+    return "F"
+  else:
+    return f"{percentage}%"
 
 if __name__ == "__main__":
     main()
