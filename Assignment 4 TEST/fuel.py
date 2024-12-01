5da72384f6d09f3237f88a412825f6ffa7e6baf1
Risha-3 <rs989@kent.ac.uk>
2024-12-01 15:08:56 +0000

    fuel.py foundations complete

--- Assignment 4 TEST/fuel.py
+++ Assignment 4 TEST/fuel.py
@@ -1,22 +1,34 @@
 def main():
   """
+  
   """
   fraction = input("Enter a fraction as X/Y: ")
-  percentage = convert(fraction)
-  gauge_reading = gauge(percentage)
-  print(f"Gauge: {gauge_reading}")
+  try:
+    percentage = convert(fraction)
+    gauge_reading = gauge(percentage)
+    print(f"Gauge: {gauge_reading}")
+  except ZeroDivisionError as e:
+    print(f"ERROR: {e}")
+  except ValueError as e:
+    print(f"ERROR: {e}")
   
 def convert(fraction):
   """
+
   """
   numerator, denominator = fraction.split("/")
   numerator = int(numerator)
   denominator = int(denominator)
+  if denominator == 0:
+    raise ZeroDivisionError("Y cannot be 0 in X/Y.")
+  if numerator > denominator:
+    raise ValueError("X must be less than or equal to Y.")
   percentage = round((numerator / denominator) * 100)
   return percentage
 
 def gauge(percentage):
   """
+  This function returns the gauge reading as a string based on the percentage.
   """
   if percentage <= 1:
     return "E" 
