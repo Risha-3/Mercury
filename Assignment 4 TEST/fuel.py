fcdcfa1ead4263427069a2fc029c1b396295545d
Risha-3 <rs989@kent.ac.uk>
2024-12-01 21:39:35 +0000

    Comments

--- Assignment 4 TEST/fuel.py
+++ Assignment 4 TEST/fuel.py
@@ -1,29 +1,38 @@
 def main():
   """
-  
+  This function asks the user to enter a fraction (X/Y).
+  Converts fraction to a percentage using the convert function
+  Using the gauge function, an appropriate gauge reading is displayed based on the percentage. 
   """
   fraction = input("Enter a fraction as X/Y: ")
   try:
+    # Calling the convert and gauge functions.
     percentage = convert(fraction)
     gauge_reading = gauge(percentage)
     print(f"Gauge: {gauge_reading}")
   except ZeroDivisionError:
+    # This message is displayed if there is a division by zero. 
     print("Y cannot be 0 in X/Y.")
   except ValueError:
+    # This message is displayed if an improper fraction or non-integers are entered.
     print("X and Y must be integers and X must be less than or equal to Y.")
   
 def convert(fraction):
   """
-
+  Convert the user input (X/Y) into a percentage
   """
+  # The input X/Y is split into numerator and denominator.
   numerator, denominator = fraction.split("/")
-  # Converting the numerator and denominator to integers, raises ValueError if it is not valid.
+  # Converting the numerator and denominator to integers. A ValueError is raised if it cannot be done.
   numerator = int(numerator)
   denominator = int(denominator)
-  if denominator == 0:
+  # If the denomiator is 0, the convert function raises a ZeroDivisionError.
+  if denominator == 0: 
     raise ZeroDivisionError
+  # If the user enters an improper fraction, the convert function raises a ValueError.
   if numerator > denominator:
     raise ValueError
+  # Calculate and return the percentage.
   percentage = round((numerator / denominator) * 100)
   return percentage
 
@@ -36,10 +45,8 @@ def gauge(percentage):
   elif percentage >= 99:
     return "F"
   else:
+    # If the percentage is greater than 1 and less than 99, the {percentage}% is returned.
     return f"{percentage}%"
 
 if __name__ == "__main__":
     main()
-
-# cd "C:\Users\risha\Documents\Mercury repo\Mercury\Assignment 4 TEST"
-# python -m pytest test_fuel.py
--- Assignment 4 TEST/test_fuel.py
+++ Assignment 4 TEST/test_fuel.py
@@ -1,31 +1,43 @@
 import pytest
 from fuel import convert, gauge
 
-# The four test cases for the convert function
-def test_convert_valid():
+# The four test cases for the convert function are below.
+# Function to test for valid fractions.
+def test_convert_normal():
     assert convert("5/10") == 50
-    assert convert("3/4") == 75
+    assert convert("0/1") == 0
 
+# Function to test for when Y in X/Y is zero.
 def test_convert_zero_division():
     with pytest.raises(ZeroDivisionError):
         convert("10/0")
 
+# Function to test for when X in X/Y is greater than Y.
 def test_convert_improper_fraction():
     with pytest.raises(ValueError):
         convert("10/9")
 
-def test_convert_letter():
+# Function to test for cases when X or Y in X/Y is not an integer.
+def test_convert_non_integer():
     with pytest.raises(ValueError):
-        convert("a/5")
+        convert("a/b")
+    with pytest.raises(ValueError):
+        convert("5.5/10")
 
-# The three test cases for the gauge function
+# The three test cases for the gauge function are below.
+# Function to test whether "E" is returned for 0% and 1%.
 def test_gauge_empty():
     assert gauge(0) == "E"
     assert gauge(1) == "E"
 
+# Function to test the mid-range percentages.
 def test_gauge_percent():
     assert gauge(50) == "50%"
 
+# Function to test whether "F" is returned for 99% and 100%.
 def test_gauge_full():
     assert gauge(99) == "F"
     assert gauge(100) == "F"
+
+# cd "C:\Users\risha\Documents\Mercury repo\Mercury\Assignment 4 TEST"
+# python -m pytest test_fuel.py
