2cb7209aa8545d239e01f306e25d8cf74c30cc6f
Risha-3 <rs989@kent.ac.uk>
2024-12-01 16:58:13 +0000

    ---

--- Assignment 4 TEST/test_fuel.py
+++ Assignment 4 TEST/test_fuel.py
@@ -3,19 +3,18 @@ from fuel import convert, gauge
 
 # The four test cases for the convert function
 def test_convert_valid():
-    assert convert("1/2") == 50
     assert convert("5/10") == 50
     assert convert("3/4") == 75
 
-def test_convert_zero_denominator():
+def test_convert_zero_division():
     with pytest.raises(ZeroDivisionError):
         convert("10/0")
 
-def test_convert_numerator_greater_than_denominator():
+def test_convert_improper_fraction():
     with pytest.raises(ValueError):
         convert("10/9")
 
-def test_convert_invalid_format():
+def test_convert_letter():
     with pytest.raises(ValueError):
         convert("a/5")
 
@@ -26,7 +25,6 @@ def test_gauge_empty():
 
 def test_gauge_percent():
     assert gauge(50) == "50%"
-    assert gauge(75) == "75%"
 
 def test_gauge_full():
     assert gauge(99) == "F"
